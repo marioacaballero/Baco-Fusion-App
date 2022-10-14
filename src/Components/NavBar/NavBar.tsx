7b227ef58b2e117e6095b89827b765c54eed6bb6
vanegabioudclear <vanesagabioud@yahoo.com.ar>
2022-10-14 18:08:22 -0300

    add functionality to navBar component

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import style from "./NavBar.module.css"
 
 export const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className={style.navBar}>
+        <Link to={"/"}>Inicio</Link>
+        <Link to={"/about"}>Sobre nosotros</Link>
+        <Link to={"/history"}>Nuestra historia</Link>
+        <Link to={"/"}>Menu</Link>
+        <Link to={"/"}>Reservas</Link>
+    </div>
   )
 }
