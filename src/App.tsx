1a4087fba259f141ce9c087610eb0d990fb6abb6
vanegabioudclear <vanesagabioud@yahoo.com.ar>
2022-10-14 17:52:13 -0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./Components/Home/Home";
+import { About } from "./Components/About/About";
+import { History } from "./Components/History/History";
+import { NavBar } from "./Components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
     </div>
   );
 }
