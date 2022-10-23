55c6d8490b398cd4785608c1c357c0371a3cbb45
Mario <mario.caballero.948@gmail.com>
2022-10-23 00:23:21 -0300

    add menu and its styles

---
+++ src/Assets/Data.tsx
@@ -0,0 +1,95 @@
+import logo from "../Baco/Menu/Logo.jpeg";
+
+export const menuFoodMeatData = [
+  {
+    name: "Lomo al estilo BACO",
+    description:
+      "Medallones de lomo, salsa de vino Malbec, pure rustico con crocantes de verdeo y jamon",
+    img: logo,
+  },
+  {
+    name: "Lomo a la Pimienta",
+    description: "Tournedo de lomo, pimienta en grano negra y papas a la crema",
+    img: logo,
+  },
+  {
+    name: "Lomo al Verdeo",
+    description:
+      "Medallones de lomo en salsa de crema de verdeo acompañado de papas cuñas",
+    img: logo,
+  },
+  {
+    name: "Bife de Campo",
+    description:
+      "Bife de chorizo, morron, cebolla asada, lechuga, berenjenas empanadas, papas fritas y huevo frito",
+    img: logo,
+  },
+  {
+    name: "Bife Timoteo",
+    description:
+      "Bife de chorizo mariposa con tomates confitados, papas cuña y rucula",
+    img: logo,
+  },
+  {
+    name: "Bife Ramon",
+    description:
+      "Bife de chorizo mariposa con una capa crocante de mani, perejil y parmesano, acompañado de una ensalada tibia de tomates y papa",
+    img: logo,
+  },
+];
+
+export const menuFoodPorkData = [
+  {
+    name: "Bondiola a la Chapa",
+    description:
+      "Bondiola, cebolla, verdeo, cerveza negra, crema de leche y papas cuñas",
+    img: logo,
+  },
+  {
+    name: "Bondiola Alemana",
+    description:
+      "Bondiola en suave salsa de limon con perejil, sobre una sutil tortilla de papa con corazon de queso",
+    img: logo,
+  },
+];
+
+export const menuFoodPastaData = [
+  {
+    name: "Ravioles de espinaca",
+    description: "con salsa",
+    img: logo,
+  },
+  {
+    name: "Sorrentinos de jamon y queso",
+    description: "con salsa",
+    img: logo,
+  },
+];
+
+export const menuFoodChickenData = [
+  {
+    name: "Pollo Tai",
+    description:
+      "Lonjas de pollo salteadas, salsa tailandesa agridulce con guarnición de verduras al wok",
+    img: logo,
+  },
+  {
+    name: "Pollo a la Calabresa",
+    description:
+      "Pollo con morrones, tomate, salsa picante acompañado de papas españolas",
+    img: logo,
+  },
+];
+
+export const menuFoodFishData = [
+  {
+    name: "Boga despinada",
+    description: "Al limón, a la pizza ó a la Pescadora, con papas fritas",
+    img: logo,
+  },
+  {
+    name: "Boga rellena con verduras (morrones, cebolla)",
+    description: "con papas fritas",
+    img: logo,
+  },
+];
