204865c3c344af3b131a74e3bec01b8bd6ad3959
federicofucci21 <federicofucci@hotmail.com>
2022-10-23 19:25:32 -0300

    hamburguer button

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -4,7 +4,9 @@ import style from "./NavBar.module.css";
 import logo from "../../Baco/Logo/LogoNav.png";
 
 export const NavBar = () => {
+
   const navigate = useNavigate();
+
   return (
     <div className={style.navBar}>
       <section>
@@ -14,11 +16,19 @@ export const NavBar = () => {
           <p>Vinos y Algo Mas</p>
         </div>
       </section>
-      <section>
+      <label htmlFor="menu" className={style.nav__label}>
+        <div className={style.btn_hmb} id="btn_hmb" >
+          <div className={style.linea1} id="linea1"></div>
+          <div className={style.linea2} id="linea2"></div>
+          <div className={style.linea3} id="linea3"></div>
+        </div>
+      </label>
+      <input type="checkbox" id="menu" className={style.nav__input} />
+      <div className={style.nav__menu}>
         <h2 onClick={() => navigate("/")}>Inicio</h2>
         <h2 onClick={() => navigate("/")}>Galeria</h2>
         <h2 onClick={() => navigate("/menu/#carnes")}>Menu</h2>
-      </section>
+      </div>
       <section>
         <h3 onClick={() => navigate("/reservations")}>Reservas</h3>
       </section>
