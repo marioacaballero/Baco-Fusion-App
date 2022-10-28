29c99fe163f1c65484aca9875c6a8b048a7217ae
vanegabioudclear <vanesagabioud@yahoo.com.ar>
2022-10-28 17:46:28 -0300

    fix navbar and home style in responsive

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -4,38 +4,49 @@ import style from "./NavBar.module.css";
 import logo from "../../Baco/Logo/LogoNav.png";
 
 export const NavBar = () => {
-
   const navigate = useNavigate();
 
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggle:Function = ()=>{
-    setIsOpen(!isOpen)
-  }
+  const toggle: Function = () => {
+    setIsOpen(!isOpen);
+  };
 
-
+  const goTo: Function = (path: String) => {
+    toggle();
+    navigate(`${path}`);
+  };
 
   return (
     <div className={style.navBar}>
       <section>
         <img alt="baco" src={logo} />
         <div>
-          <h2>Baco Fusion</h2>
-          <p>Vinos y Algo Mas</p>
+          <h2>Baco Fusión</h2>
+          <p>Vinos y Algo Más</p>
         </div>
       </section>
-      <label htmlFor="menu" onClick={() => toggle()} className={style.nav__label}>
-        <div className={style.btn_hmb} id="btn_hmb" >
+      <label
+        htmlFor="menu"
+        onClick={() => toggle()}
+        className={style.nav__label}
+      >
+        <div className={style.btn_hmb} id="btn_hmb">
           <div className={style.linea1} id="linea1"></div>
           <div className={style.linea2} id="linea2"></div>
           <div className={style.linea3} id="linea3"></div>
         </div>
       </label>
-      <input type="checkbox" id="menu" defaultChecked={isOpen?true:false} className={style.nav__input} />
+      <input
+        type="checkbox"
+        id="menu"
+        checked={isOpen}
+        className={style.nav__input}
+      />
       <div className={style.nav__menu}>
-        <h2 onClick={() => navigate("/")}>Inicio</h2>
-        <h2 onClick={() => navigate("/gallery")}>Galeria</h2>
-        <h2 onClick={() => navigate("/menu/#carnes")}>Menu</h2>
+        <h2 onClick={() => goTo("/")}>Inicio</h2>
+        <h2 onClick={() => goTo("/gallery")}>Galeria</h2>
+        <h2 onClick={() => goTo("/menu/#carnes")}>Menu</h2>
       </div>
       <section>
         <h3 onClick={() => navigate("/reservations")}>Reservas</h3>
