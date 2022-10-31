3fe78442d4ff6e9ff1672618447ec8a3e50add20
vanegabioudclear <vanesagabioud@yahoo.com.ar>
2022-10-31 14:44:47 -0300

    fix stle on maps component and add responsive style too

--- src/Components/GoogleMaps/Maps.tsx
+++ src/Components/GoogleMaps/Maps.tsx
@@ -1,28 +1,13 @@
 import React from "react";
-
-{/* <iframe
-  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1415.0843671994046!2d-58.222639285836976!3d-32.481870362801715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa6bf504561d387af!2sBaco%20Fusi%C3%B3n%20Restaurant!5e0!3m2!1sen!2sar!4v1667231803931!5m2!1sen!2sar"
-  width="600"
-  height="450"
-  style="border:0;"
-  allowfullscreen=""
-  loading="lazy"
-  referrerpolicy="no-referrer-when-downgrade"
-></iframe>; */}
+import style from "./Maps.module.css";
 
 function Maps() {
   return (
-    <div>
+    <div className={style.mapsDiv}>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1415.0843671994046!2d-58.222639285836976!3d-32.481870362801715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa6bf504561d387af!2sBaco%20Fusi%C3%B3n%20Restaurant!5e0!3m2!1sen!2sar!4v1667231803931!5m2!1sen!2sar"
-        width="600"
-        height="450"
-        style={{ border: "0px" }}
-        // allowfullscreen=""
         loading="lazy"
-        // referrerpolicy="no-referrer-when-downgrade"
       ></iframe>
-      ;
     </div>
   );
 }
