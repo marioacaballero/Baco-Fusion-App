f21c8ef4e03f32434e89b8f2593c9063418d4a91
federicofucci21 <federicofucci@hotmail.com>
2022-11-01 21:33:35 -0300

    add logo, devs, instagram, facebook, to footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,6 +1,39 @@
 import React from "react";
 import style from "./Footer.module.css";
+import logo from "../../Baco/Logo/LogoNav.png";
+import { BsFacebook } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
 
 export const Footer = () => {
-  return <div className={style.footer}>Footer</div>;
+
+  function Copyright() {
+    return (
+
+      <h5>{"Copyright © " + new Date().getFullYear()}</h5>
+    
+    );
+}
+
+  return (
+    <div className={style.footer}>
+      <div>
+        <h5>Powered by</h5>
+        <h3>CaneDev</h3>
+      </div>
+      <div className={style.brand}>
+        <img alt="baco" src={logo} />
+        <Copyright />
+        {/* <h2>Baco Fusión</h2>
+        <p>Vinos y Algo Más</p> */}
+      </div>
+      <div className={style.redes}>
+        <a href="https://www.facebook.com/BacoFusion/" target="_blank" rel="noreferrer">
+          <BsFacebook  style={{ fontSize: 35 }} />
+        </a>
+        <a href="https://www.instagram.com/bacofusion/?hl=es" target="_blank" rel="noreferrer">
+          <BsInstagram  style={{ fontSize: 35 }} />
+        </a>
+      </div>
+    </div>
+  );
 };
