8e2b8c9176528506668e8757815bd3d3c6b5f550
Mario <mario.caballero.948@gmail.com>
2022-11-10 21:54:40 -0300

    fix responsive style on footer component

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import style from "./Footer.module.css";
-import logo from "../../Baco/Logo/LogoNav.png";
 import { BsFacebook, BsInstagram } from "react-icons/bs";
 import { FaMapMarkedAlt, FaPhoneSquareAlt } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
@@ -17,16 +16,11 @@ export const Footer = () => {
   return (
     <div className={style.footer}>
       <section>
-        {/* <div>
-        <h5>Powered by</h5>
-        <h3>CaneDev</h3>
-      </div> */}
         <div
           className={style.brand}
           onClick={() => goTo("/")}
           style={{ cursor: "pointer" }}
         >
-          {/* <img alt="baco" src={logo} /> */}
           <h2>Baco Fusión</h2>
           <p>Vinos y Algo Más</p>
         </div>
@@ -34,7 +28,7 @@ export const Footer = () => {
           <h3>Contacto</h3>
           <div>
             <div>
-              <FaPhoneSquareAlt style={{ fontSize: 22 }} />
+              <FaPhoneSquareAlt className={style.contactIcon} />
               <h4>(5493442) 549695</h4>
             </div>
             <a
@@ -48,7 +42,7 @@ export const Footer = () => {
         <div className={style.point}>
           <h3>Ubicación</h3>
           <div>
-            <FaMapMarkedAlt style={{ fontSize: 30 }} />
+            <FaMapMarkedAlt className={style.pointIcon} />
             <a
               href="https://www.google.com.ar/maps/place/Baco+Fusi%C3%B3n+Restaurant/@-32.4814829,-58.2249961,17z/data=!3m1!4b1!4m5!3m4!1s0x95afdb00f6630b2b:0xa6bf504561d387af!8m2!3d-32.4814874!4d-58.2228074"
               target="_blank"
@@ -58,26 +52,26 @@ export const Footer = () => {
           </div>
         </div>
         <div className={style.social}>
-          <h3>Siguenos en</h3>
+          <h3>Seguinos en</h3>
           <div className={style.redes}>
             <a
               href="https://www.facebook.com/BacoFusion/"
               target="_blank"
               rel="noreferrer"
             >
-              <BsFacebook style={{ fontSize: 35 }} />
+              <BsFacebook className={style.socialIcon} />
             </a>
             <a
               href="https://www.instagram.com/bacofusion/?hl=es"
               target="_blank"
               rel="noreferrer"
             >
-              <BsInstagram style={{ fontSize: 35 }} />
+              <BsInstagram className={style.socialIcon} />
             </a>
           </div>
         </div>
       </section>
-      <Copyright/>
+      <Copyright />
     </div>
   );
 };
