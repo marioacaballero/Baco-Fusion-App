6281b0ade5284ee9f79e4e4aae21dac7f68e47e4
Mario <mario.caballero.948@gmail.com>
2022-11-20 18:45:32 -0300

    add imgs to gallery and menu

--- src/Assets/Data.tsx
+++ src/Assets/Data.tsx
@@ -1,6 +1,38 @@
 import logo from "../Baco/Menu/Logo.jpeg";
 import lomoBaco from "../Baco/Menu/LomoBaco.jpg";
 import lomoPimienta from "../Baco/Menu/LomoPimienta.jpg";
+import bondiChapa from '../Baco/Menu/Bondiola a la chapa.jpg'
+import bifeTimo from '../Baco/Menu/bifeTimoteo.jpg'
+import polloCalabresa from '../Baco/Menu/polloCalabresa.jpg'
+import bogaPescadora from '../Baco/Menu/bocaPesacadora.jpg'
+import bogaRellena from '../Baco/Menu/bogaRellena.jpg'
+import ravioles from '../Baco/Menu/ravioles.jpg'
+import bifeCampo from '../Baco/Menu/BifeCampo.jpg'
+import polloTai from '../Baco/Menu/polloTai.jpg'
+import sorrentinos from '../Baco/Menu/sorrentinos.jpg'
+import bifeRamon from '../Baco/Menu/bifeRamon.jpg'
+import lomoVerdeo from '../Baco/Menu/lomoVerdeo.jpg'
+import bondiAlemana from '../Baco/Menu/bondiAlemana.jpg'
+import baco1 from "./Imgs/baco1.jpg";
+import baco2 from "./Imgs/baco2.jpg";
+import baco3 from "./Imgs/baco3.jpg";
+import baco4 from "./Imgs/baco4.jpg";
+import baco5 from "./Imgs/baco5.jpg";
+import baco6 from "./Imgs/baco6.jpg";
+import baco7 from "./Imgs/baco7.jpg";
+import baco8 from "./Imgs/baco8.jpg";
+import baco9 from "./Imgs/baco9.jpg";
+import baco10 from "./Imgs/baco10.jpg";
+import baco11 from "./Imgs/baco11.jpg";
+import baco12 from "./Imgs/baco12.jpg";
+import baco13 from "./Imgs/baco13.jpg";
+import baco14 from "./Imgs/baco14.jpg";
+import baco15 from "./Imgs/baco15.jpg";
+import baco16 from "./Imgs/baco16.jpg";
+import baco17 from "./Imgs/baco17.jpg";
+import baco18 from "./Imgs/baco18.jpg";
+import baco19 from "./Imgs/baco19.jpg";
+import baco20 from "./Imgs/baco20.jpg";
 
 export const menuFoodMeatData = [
   {
@@ -18,25 +50,25 @@ export const menuFoodMeatData = [
     name: "Lomo al Verdeo",
     description:
       "Medallones de lomo en salsa de crema de verdeo acompañado de papas cuñas",
-    img: logo,
+    img: lomoVerdeo,
   },
   {
     name: "Bife de Campo",
     description:
       "Bife de chorizo, morron, cebolla asada, lechuga, berenjenas empanadas, papas fritas y huevo frito",
-    img: logo,
+    img: bifeCampo,
   },
   {
     name: "Bife Timoteo",
     description:
       "Bife de chorizo mariposa con tomates confitados, papas cuña y rucula",
-    img: logo,
+    img: bifeTimo,
   },
   {
     name: "Bife Ramon",
     description:
       "Bife de chorizo mariposa con una capa crocante de mani, perejil y parmesano, acompañado de una ensalada tibia de tomates y papa",
-    img: logo,
+    img: bifeRamon,
   },
 ];
 
@@ -45,13 +77,13 @@ export const menuFoodPorkData = [
     name: "Bondiola a la Chapa",
     description:
       "Bondiola, cebolla, verdeo, cerveza negra, crema de leche y papas cuñas",
-    img: logo,
+    img: bondiChapa,
   },
   {
     name: "Bondiola Alemana",
     description:
       "Bondiola en suave salsa de limon con perejil, sobre una sutil tortilla de papa con corazon de queso",
-    img: logo,
+    img: bondiAlemana,
   },
 ];
 
@@ -59,12 +91,12 @@ export const menuFoodPastaData = [
   {
     name: "Ravioles de espinaca",
     description: "Con salsa",
-    img: logo,
+    img: ravioles,
   },
   {
     name: "Sorrentinos de jamon y queso",
     description: "Con salsa",
-    img: logo,
+    img: sorrentinos,
   },
 ];
 
@@ -73,13 +105,13 @@ export const menuFoodChickenData = [
     name: "Pollo Tai",
     description:
       "Lonjas de pollo salteadas, salsa tailandesa agridulce con guarnición de verduras al wok",
-    img: logo,
+    img: polloTai,
   },
   {
     name: "Pollo a la Calabresa",
     description:
       "Pollo con morrones, tomate, salsa picante acompañado de papas españolas",
-    img: logo,
+    img: polloCalabresa,
   },
 ];
 
@@ -87,11 +119,36 @@ export const menuFoodFishData = [
   {
     name: "Boga despinada",
     description: "Al limón, a la pizza ó a la Pescadora, con papas fritas",
-    img: logo,
+    img: bogaPescadora,
   },
   {
     name: "Boga rellena con verduras (morrones, cebolla)",
     description: "Con papas fritas",
-    img: logo,
+    img: bogaRellena,
   },
 ];
+
+export const galleryImgs = [
+  baco1,
+  baco2,
+  baco3,
+  baco4,
+  baco5,
+  baco6,
+  baco7,
+  baco8,
+  baco9,
+  baco10,
+  baco11,
+  baco12,
+  baco13,
+  baco14,
+  baco15,
+  baco16,
+  baco17,
+  baco18,
+  baco19,
+  baco20,
+];
+
+
--- src/Components/Gallery/Gallery.tsx
+++ src/Components/Gallery/Gallery.tsx
@@ -1,31 +1,8 @@
 import React, { useState } from "react";
-// import style from "./Galery.module.css";
 import { GalleryImage } from "./GalleryImage";
 import { GalleryModal } from "./GalleryModal";
 import "./Gallery.css";
-import { useHref, useNavigate } from "react-router-dom";
-
-let imgUrls = [
-  "https://scontent.faep14-3.fna.fbcdn.net/v/t39.30808-6/271875616_3029772800629830_6123713048690336497_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeEyNBTXsIP5F8EvP1PMNvfzWy7agCSCDwhbLtqAJIIPCC9jpesjX3h7K6Jy_U46jIoLhhrJVWgL7j-BD2VCUNB9&_nc_ohc=h4BqdRI-PbEAX_A4zVP&_nc_ht=scontent.faep14-3.fna&oh=00_AT8epqUFWPME3p_AQQaouN4K7fKZF2kox1vWfiTTDlnaRA&oe=635E7803",
-  "https://scontent.faep14-3.fna.fbcdn.net/v/t39.30808-6/271934755_3029773587296418_3561490144716509737_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFiohP6I4zfUHOwlkwHupBCTkt1-uvcJ3dOS3X669wnd8SU_8HsdxCMHAJYS1ArrqJzIYhp422OtoQpf8EyzN_9&_nc_ohc=ISLGDWx_UvUAX-0zvok&_nc_ht=scontent.faep14-3.fna&oh=00_AT_CNclBg-sn6mkDe6FsQvyPI0JfNxur_MdltABWoV4GvQ&oe=635E3132",
-  "https://scontent.faep14-3.fna.fbcdn.net/v/t1.6435-9/89597223_2518012008472581_652152764847095808_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=9267fe&_nc_eui2=AeFd58F7Z95UCfOiolwlY4yuwldF3LW9HVDCV0Xctb0dUP8GxQwAvqVZcen6u1207u2Gbeb6ZhHFt8F5u6h0fFjm&_nc_ohc=KwYn2C6OfP0AX8y7hq9&_nc_ht=scontent.faep14-3.fna&oh=00_AT-OVK5Lp7NLIhYyfh9XMQND8VjqDtffvmhMXmPN7kmcAQ&oe=637F8F72",
-  "https://scontent.faep14-3.fna.fbcdn.net/v/t1.6435-9/82838886_2481871202086662_7832198414187102208_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeGK-pv0SQPx6Ddms19frQItCGctZWzIYucIZy1lbMhi5x3yNi7CwnZb4-VJqAzDWCNx-T6laKD4_ZXfDqTtu0ug&_nc_ohc=0TeFjG9IftkAX8ZS_O_&_nc_ht=scontent.faep14-3.fna&oh=00_AT9LSNTsAuyTOTfjIGIIfk0F0mpGelnYpXA3XEacQkflzQ&oe=637CDD2B",
-  "https://scontent.faep14-2.fna.fbcdn.net/v/t1.6435-9/83090070_2481871155420000_2702689680897343488_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeEoPRmaEW0vbK7fsgR2DWwoxdTYpKEPfDnF1NikoQ98OYsp02tuxm6R8BQNelHGF30PXJS3NHeUFqh-oc_xhryA&_nc_ohc=NwSMEawk4qUAX8Zz_W8&_nc_ht=scontent.faep14-2.fna&oh=00_AT_kH7cEtqoJT5ZeZi4jbVvEiwcwJPqB3QWSrq9bbyTIhw&oe=638081B2",
-  "https://scontent.faep14-2.fna.fbcdn.net/v/t1.6435-9/82901518_2481871068753342_4414354106789920768_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeEEQvtkOJGqXQIT_GWojjYSCy4CG9K9X6QLLgIb0r1fpF1yhMky9-QZFyEywfEXkJ9mU3vSS0280-bf8goGRI9I&_nc_ohc=23U6RHxbYp8AX8bWmYE&_nc_ht=scontent.faep14-2.fna&oh=00_AT8tZLYEg7XbMdzV1qj4FRk4CxAKKAmVMIyXbWAmAG2vrw&oe=637E4E31",
-  "https://scontent.faep14-3.fna.fbcdn.net/v/t1.6435-9/62529784_2300858573521260_8355671759822782464_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeGvKqtVnp7w7gpiY0WrvTYIXI7GL5ObVqBcjsYvk5tWoH6LD5TTxskDIbr-ObYat8buBocGDErhf0qRhrPJ3gM7&_nc_ohc=IqjlATawAEgAX8SoS5T&_nc_ht=scontent.faep14-3.fna&oh=00_AT-q4gAZlya7yNknNDn6kx4SOB667lpXFhLPf_u8YNW-1Q&oe=637F07A1",
-  "https://scontent.faep14-3.fna.fbcdn.net/v/t1.6435-9/61273403_2289858767954574_3923786932265418752_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHgtmDQH2R_cjrG3jFu1zpD5S1Qgdn-BZzlLVCB2f4FnCU6yv9pv8lmql_-8GYofWjyv3Q0zqy7TZccolgNDaGa&_nc_ohc=u7_NEYjmnLkAX9KPEiy&_nc_ht=scontent.faep14-3.fna&oh=00_AT8HUlt-tHmCriQMAW8TdgyHMzJuS4PVxfTHsclqRa_wjA&oe=637E626D",
-  "https://scontent.faep14-2.fna.fbcdn.net/v/t1.6435-9/61455204_2289858744621243_6836151195657043968_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHL70ONRZPQBuGjTT0CObe9sY3Hlu4iXTKxjceW7iJdMnjOF-Y9sVJi3qY8-1E4Oogz4h9z6dn7zNcjv1Ja4URd&_nc_ohc=Oy0YsK8JuqEAX-N49qe&_nc_oc=AQlpqZY_LEEaeJblFkgMiEA40uFA0PCXDDzN6yqRk93X7SDZTTakm2vCb-b5s-IBJiM&_nc_ht=scontent.faep14-2.fna&oh=00_AT8Oxx_zoa7NKm56fDmnfzrduWYSCOeVzCfgOL9ompF_0w&oe=637E22DE",
-  "https://scontent.faep14-2.fna.fbcdn.net/v/t1.6435-9/61324942_2289858714621246_3822953250617819136_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHgZs4Fd-D9Ur4b2gMEEvdkOAp_ZygXldI4Cn9nKBeV0ongvV_m8pD59a7LpzP_PfY6QUjvU9F4FFWcT1ZHb5X5&_nc_ohc=uBL7_B1xxQ8AX8_Uzn7&_nc_ht=scontent.faep14-2.fna&oh=00_AT8uQYF6lMW8qCcN3LB9r4ObzaQmnbreTIo1nXDl4OBTHw&oe=63804D62",
-  "https://scontent.faep14-2.fna.fbcdn.net/v/t1.6435-9/61376187_2289858607954590_1508775986215256064_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeEPUH8W3GCH918iivP-A0h2_Sb_G5KR87z9Jv8bkpHzvPixdqdlccAFUmhVbyap-V9x5dqWWVMccfsDcpZT2yph&_nc_ohc=gC8_sgJrkY8AX9zx-zM&_nc_ht=scontent.faep14-2.fna&oh=00_AT827dL_byVVOrKbKlg2OImphVDh-ccAEG0gKJMXTamzeQ&oe=63805E61",
-  "https://scontent.faep14-2.fna.fbcdn.net/v/t1.6435-9/61950744_2289858587954592_3246098035633029120_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGmENSy1JKX1Qhe_kijUDHPSDDYeg414sxIMNh6DjXizPIduhsuqHjpmwhhzNCXM2XyctKJO6Th6Puf3m5vBomV&_nc_ohc=3gRcOJgQ2S0AX-AKFlP&tn=-JFiPmrX6MuF-QyW&_nc_ht=scontent.faep14-2.fna&oh=00_AT-UV4hB6mOx_jWP7OkmPQT7I0BNa5pdDFAKO0CfPBlK3A&oe=637F249A",
-  "https://scontent.faep14-2.fna.fbcdn.net/v/t1.6435-9/61703728_2289858561287928_401077463578312704_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHOJIoenrbh3N5329DDn1HKKWBlgD_z_dUpYGWAP_P91Xc3C3gceo4LE6MU__VMfOpH9WuZAP6uVRMHxYwnbqwE&_nc_ohc=X4WUoOBLeJUAX_dNCsc&_nc_ht=scontent.faep14-2.fna&oh=00_AT8stI8b-pEyOBNIgt0YLNXB5DBUEHG63jM7uGcHD7vh-g&oe=637D50B0",
-  "https://scontent.faep14-3.fna.fbcdn.net/v/t1.6435-9/61516724_2289858494621268_2739730075858501632_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHXkT6YRwTlpjirZvN7vSewcEE49Rz_-yNwQTj1HP_7I96WFRuO8cRCbeC-xqrA9GHZwZNPlQzM_7AvWvOYXDPV&_nc_ohc=EJZfb6LAMzIAX9rn7QA&_nc_ht=scontent.faep14-3.fna&oh=00_AT-F739QB4LsDHHQs9AZjqtLdSJ5YPRahmYChHw54UrYEA&oe=637E87FA",
-  "https://scontent.faep14-3.fna.fbcdn.net/v/t1.6435-9/61999486_2289858381287946_3156812825259671552_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFqVnz__I9OU1_wv6ii75cOh2C8a9mFnNWHYLxr2YWc1c2hTDAeSAt-r4MOQ-4n3Tfi58D6f6jIQ5P3FlWGtf_d&_nc_ohc=iRl2_iwvnSYAX-zn-qG&_nc_ht=scontent.faep14-3.fna&oh=00_AT9BSefW9pVi8QgTgFUTMUqCAeXRRPUeOY2T9yCiEKI0oQ&oe=637E81B9",
-  "https://scontent.faep14-3.fna.fbcdn.net/v/t1.6435-9/61739561_2289858357954615_4655303683355967488_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFbZ1fXAviuSvoPt28lZiJSoza5mWf_sGujNrmZZ_-wa6Lj1tah_GQ8maYIyiXjtunHPB6e_bfBDIDNRetdBTNn&_nc_ohc=ikU5zj_0_XgAX-RsikF&_nc_ht=scontent.faep14-3.fna&oh=00_AT-2QemHLM6hYudq8PG98wkoaurCuhTvk-48hvtIN_hOTg&oe=637EB311",
-  "https://scontent.faep14-2.fna.fbcdn.net/v/t1.6435-9/61706529_2289858281287956_8658387033439338496_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeF2Az7_6Bg5vbiNvnkb35rENGBtSh633GY0YG1KHrfcZtJtm8lpE2ifCIAjepSWaCV5sxLi8nEHU2sRbFdVf50t&_nc_ohc=bfi-WV4dM4AAX93-xr3&_nc_ht=scontent.faep14-2.fna&oh=00_AT-mWQ0ftIU192eVyQUGjsNIqv-k1EivfT3RA2xBvtWUVQ&oe=63809345",
-  "https://scontent.faep14-3.fna.fbcdn.net/v/t1.6435-9/61002486_2283845721889212_481585500260401152_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeEH56eGz3UD5ap-YLZEAzr_6gr2wKoJqhDqCvbAqgmqEFRr5OonKNh5MxtOqmg6pwjAoGyqw6YMVNeJu9mS6svU&_nc_ohc=jyDF9PUw_0MAX-TC1__&_nc_oc=AQkfEEoOYd8knriniKDiGATSgW2VB0nk_Qco_zznwd-WahZV_9fiQcuoItZdsOHsjKc&_nc_ht=scontent.faep14-3.fna&oh=00_AT97k-S3duhfwIXSobMhFJ0KZsYYybM-ShToA9I-uNQvMA&oe=637D413F",
-  "https://scontent.faep14-2.fna.fbcdn.net/v/t1.6435-9/60766937_2283845348555916_7029674364656156672_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeHL8RZMcarl91ffMP0WqI77vlrbUgGbscu-WttSAZuxy4LXt60m7tbAm10zyTGpzp8l2Jz_48sL6m9-EV49cS8P&_nc_ohc=Yp7AnDFzb6MAX8BTnWC&_nc_ht=scontent.faep14-2.fna&oh=00_AT-9KJ9z73hYWeuUzRx8Q3K1NtP215hhmZIZ2bMcWjqoTQ&oe=637DE23C",
-];
+import { galleryImgs } from "../../Assets/Data";
 
 export const Gallery = () => {
   const [isOpen, setIsOpen] = useState({
@@ -39,7 +16,7 @@ export const Gallery = () => {
       isOpen: true,
       url: url,
     });
-    window.location.assign('/gallery/#top')
+    window.location.assign("/gallery/#top");
   };
 
   // Function for closing modal dialog
@@ -53,7 +30,7 @@ export const Gallery = () => {
   return (
     <div className="gallery-container">
       <div className="row">
-        {imgUrls.map((url, index) => {
+        {galleryImgs.map((url, index) => {
           return (
             <div key={index} className="col-sm-6 col-md-3 col-xl-2">
               <div className="gallery-card">
@@ -78,7 +55,7 @@ export const Gallery = () => {
           show={isOpen.isOpen}
           onClick={closeModal}
           src={isOpen.url}
-          id={'top'}
+          id={"top"}
         />
       ) : (
         <></>
